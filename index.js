f12a6eb78365bfbe830d3dcaca06ca2f5ecb5d04
Geno M <giantpowpow2@gmail.com>
2019-07-29 01:55:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ function destructivelyRemoveFirstKitten() {
   return kittens.shift();
 }
 
-function appendKitten(nameOfNewKitten) {
-  return kittens.concat(nameOfNewKitten);
-}
-
 /* function appendKitten(nameOfNewKitten) {
+  return kittens.concat(nameOfNewKitten);
+} 
+*/
+
+function appendKitten(nameOfNewKitten) {
   return [...kittens, nameOfNewKitten];
 }
-*/
 
 function prependKitten(nameOfNewKitten) {
   return [nameOfNewKitten, ...kittens];
