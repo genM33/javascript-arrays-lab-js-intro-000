8ce7a55c84954154794bb91d229dc11b45af3467
Geno M <giantpowpow2@gmail.com>
2019-07-29 01:28:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(nameOfNewKitten) {
+  return kittens.push(nameOfNewKitten);
+}
+
+function destructivelyPrependKitten(nameOfNewKitten) {
+  return kittens.unshift(nameOfNewKitten);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
